ed3b6d7c4f99af2d6c8316da6feefaa80f2f96ee
adpaul3676 <adpaul3676@gmail.com>
2021-05-01 15:48:35 -0600

    Reformatted the edit fields

--- proj_nodb/src/components/displayButtons.js
+++ proj_nodb/src/components/displayButtons.js
@@ -8,43 +8,24 @@ class DisplayButtons extends Component {
       textInput: '',
       titleInput: '',
     }
-    this.callFunctions = this.callFunctions.bind (this)
+    // this.callFunctions = this.callFunctions.bind (this)
     this.editInitializeFunction = this.editInitializeFunction.bind (this)
-    this.handleTextChange = this.handleTextChange.bind (this)
-    this.handleTitleChange = this.handleTitleChange.bind (this)
-  }
-  
-  callFunctions () {
-    this.props.editNote(this.state.textInput, this.state.titleInput, this.props.selectedNote[0].id);
-    this.props.toggleEdit();
-    this.setState({textInput: '', titleInput: ''})
+    // this.handleTextChange = this.handleTextChange.bind (this)
+    // this.handleTitleChange = this.handleTitleChange.bind (this)
   }
   
   editInitializeFunction (){
     if (this.props.selectedNote.length !== 0) {
       this.props.toggleEdit();
-      this.setState({textInput: this.props.selectedNote[0].text, titleInput: this.props.selectedNote[0].title})
+      this.props.initializeEdit()
     } else {
       alert ('You need to select a note to edit!')
     }
   }
 
-  handleTitleChange (input) {
-    this.setState({titleInput: input})
-  }
-
-  handleTextChange (input) {
-    this.setState({textInput: input})
-  }
-  
   render () {
 
-    let inputFields = <section>
-      <input placeholder='Updated Title Here' value={this.state.titleInput} onChange={(e) => this.handleTitleChange(e.target.value)} className='editTitleField'></input>
-      <input placeholder='Updated Text Here' value={this.state.textInput} onChange={(e) => this.handleTextChange(e.target.value)} className='editTextField'></input>
-    </section>
-
-    let saveButton = <button onClick={() => this.callFunctions()} className='saveButton'>Save Changes</button>
+    let saveButton = <button onClick={() => this.props.callFunctions()} className='saveButton'>Save Changes</button>
 
     let editButton = <button onClick={() => this.editInitializeFunction()} className='editButton'>Edit Note</button>
 
@@ -59,7 +40,6 @@ class DisplayButtons extends Component {
     return (
       <section>
         {this.props.toggleEditValue ? saveButton : editButton}
-        {this.props.toggleEditValue ? inputFields : null}
         {deleteButton}
       </section>
     )
--- proj_nodb/src/components/notepadDisplay.js
+++ proj_nodb/src/components/notepadDisplay.js
@@ -8,8 +8,14 @@ class NotepadDisplay extends Component {
     this.state = {
       titleInput: '',
       textInput: '',
+      textInput2: '',
+      titleInput2: '',
     }
-    this.callAll = this.callAll.bind (this);  
+    this.callAll = this.callAll.bind (this); 
+    this.handleTextChange2 = this.handleTextChange2.bind (this) 
+    this.handleTitleChange2 = this.handleTitleChange2.bind (this)
+    this.callFunctions = this.callFunctions.bind (this)
+    this.initializeEdit = this.initializeEdit.bind (this) 
   }
 
   handleTitleChange (input) {
@@ -20,10 +26,29 @@ class NotepadDisplay extends Component {
     this.setState ({textInput: input})
   } 
 
+  handleTitleChange2 (input) {
+    this.setState({titleInput2: input})
+  }
+
+  handleTextChange2 (input) {
+    this.setState({textInput2: input})
+  }
+  
+
   callAll (title, text) {
     this.props.newNote(title, text);
     this.props.createMode();
     this.setState({titleInput: '', textInput: ''})
+  }
+
+  initializeEdit () {
+    this.setState({textInput2: this.props.selectedNote[0].text, titleInput2: this.props.selectedNote[0].title})
+  }
+
+  callFunctions () {
+    this.props.editNote(this.state.textInput2, this.state.titleInput2, this.props.selectedNote[0].id);
+    this.props.toggleEdit();
+    this.setState({textInput2: '', titleInput2: ''})
   }
 
   render () {
@@ -45,6 +70,7 @@ class NotepadDisplay extends Component {
             <header className='noteTitle'>
               <h1 className='titleText'>{e.title}</h1>
             </header>
+            
             <div className='separator1'></div>
             <div className='noteBody'>{e.text}</div>
           </section>
@@ -56,11 +82,22 @@ class NotepadDisplay extends Component {
     let createButton = <button onClick={() => this.callAll(this.state.titleInput, this.state.textInput)}>Create</button>
     let backButton = <button onClick={() => this.props.createMode()}>Back</button>
 
+    let inputFields = <section>
+      <header className='noteTitle'>
+        <input placeholder='Updated Title Here' value={this.state.titleInput2} onChange={(e) => this.handleTitleChange2(e.target.value)} className='createTitleField'></input>
+      </header>
+      <div className='separator1'></div>
+      <section className='noteBody'>
+        <input placeholder='Updated Text Here' value={this.state.textInput2} onChange={(e) => this.handleTextChange2(e.target.value)} className='createTextField'></input>
+      </section>
+    </section>
+
     return (
       <section className='displayFrame'>
         <div className='viewPort'>
-        {displayNormal}
-        {this.props.toggleCreate ? displayNew : null}
+          {displayNormal}
+          {this.props.toggleCreate ? displayNew : null}
+          {this.props.toggleEditValue ? inputFields : null} 
         </div>
         <section className='buttonBox'>
           {/* <div className='bottomButtonSpacing'> */}
@@ -72,6 +109,8 @@ class NotepadDisplay extends Component {
             selectedNote = {this.props.selectedNote}
             toggleEdit = {this.props.toggleEdit}
             toggleEditValue = {this.props.toggleEditValue}
+            initializeEdit = {this.initializeEdit}
+            callFunctions = {this.callFunctions}
             />
           {/* </div> */}
         </section>
